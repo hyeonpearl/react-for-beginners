fcf29abf6b1f53290bb816beec55c8ed361105c8
hyeonpearl <hc9064@gmail.com>
2022-10-30 19:29:25 +0900

    Using useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import Button from './Button';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState('');
+  const onClick = () => setValue(prev => prev + 1);
+  const onChange = e => setKeyword(e.target.value);
+  console.log('Run all the time');
+  useEffect(() => {
+    console.log('CALL THE API...');
+  }, []);
+  useEffect(() => {
+    console.log('SEARCH FOR', keyword);
+  }, [keyword]);
   return (
     <div>
-      <h1>Welcome Back!</h1>
-      <Button text={'Click'} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type='text'
+        placeholder='Enter here'
+      ></input>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click!</button>
     </div>
   );
 }
