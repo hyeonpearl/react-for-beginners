622cabb0abad7add6ce67ae0ef4cc12adeb992dd
hyeonpearl <hc9064@gmail.com>
2022-10-31 19:30:52 +0900

    Simple To Do List

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import { useState, useEffect } from 'react';
-
-function Hello() {
-  useEffect(() => {
-    console.log('hi');
-    return () => {
-      console.log('bye');
-    };
-  }, []);
-  return <h1>Hello</h1>;
-}
+import { useState } from 'react';
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing(prev => !prev);
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+  const onChange = e => setToDo(e.target.value);
+  const onSubmit = e => {
+    e.preventDefault();
+    if (toDo === '') {
+      return;
+    }
+    setToDos(currentArray => [toDo, ...currentArray]);
+    setToDo('');
+  };
+  const removeBtn = key => {
+    setToDos(currentToDos =>
+      currentToDos.filter((_, currentKey) => currentKey !== key)
+    );
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type='text'
+          placeholder='Enter to do'
+        ></input>
+        <button>Add to do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((toDo, key) => (
+          <li key={key}>
+            {toDo}
+            <button onClick={() => removeBtn(key)}>X</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
