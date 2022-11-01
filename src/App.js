7897d6937276e0d603b42958b25312b1ff7d135b
hyeonpearl <hc9064@gmail.com>
2022-11-01 14:48:37 +0900

    Movie App

--- src/App.js
+++ src/App.js
@@ -1,56 +1,39 @@
 import { useEffect, useState } from 'react';
+import Movie from './Movie';
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [cost, setCost] = useState(1);
-  const [need, setNeed] = useState(1);
-  const onChange = event => {
-    setCost(event.target.value);
-    setNeed(1);
-  };
-  const handleInput = event => {
-    setNeed(event.target.value);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+
+    setMovies(json.data.movies);
+    setLoading(false);
   };
   useEffect(() => {
-    fetch('https://api.coinpaprika.com/v1/tickers')
-      .then(response => response.json())
-      .then(json => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
   return (
     <div>
-      <h1>Coin Tracker {loading ? '' : `(${coins.length})`}</h1>
       {loading ? (
-        <strong>loading...</strong>
+        <h2>Loading...</h2>
       ) : (
-        <select onChange={onChange}>
-          <option>Select Coin!</option>
-          {coins.map((coin, index) => (
-            <option
-              key={index}
-              value={coin.quotes.USD.price}
-              id={coin.symbol}
-              symbol={coin.symbol}
-            >
-              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
-            </option>
+        <div>
+          {movies.map(movie => (
+            <Movie
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
           ))}
-        </select>
+        </div>
       )}
-      <h2>Please enter the amount.</h2>
-      <div>
-        <span>$ </span>
-        <input
-          type='number'
-          value={need}
-          onChange={handleInput}
-          placeholder='Enter the USD'
-        />
-      </div>
-      <h2>You can get {need / cost} coin.</h2>
     </div>
   );
 }
